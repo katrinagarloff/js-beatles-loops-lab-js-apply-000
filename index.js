d5ec432352022e1b05a22fe79bab2d4157e6571d
katrinagarloff@gmail.com <katrinagarloff@gmail.com>
2018-04-12 23:51:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musAr, insAr) {
+  var musPlay = []
+  for(var i = 0; i<=musAr.length-1; i++) {
+      musPlay[i] = "" + musAr[i] + " plays " + insAr[i]
+    }
+  return musPlay
+}
+
+function johnLennonFacts(factsAr) {
+  var i = 0;
+  while (i < factsAr.length) {
+    factsAr[i] += "!!!"
+    i++
+  }
+}
